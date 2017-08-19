1bf029fb88cc6d441e24b9f26388a819b93d869c
Rahul Gosain <gosain.r@gmail.com>
2017-08-19 10:42:52 +0530

    Change time style to match mockup

--- ppb-magnus-child/partials/skill.php
+++ ppb-magnus-child/partials/skill.php
@@ -2,16 +2,22 @@
 // $skill_term names are of the form "{SkillName}[ :: {Short,Medium,Long}]"
 // but any value can be in the second position and gets converted to a CSS
 // class
-$skill_term = get_query_var('skill_term'); 
-
+$skill_term = get_query_var('skill_term');
 $components = explode(' :: ', $skill_term->name);
-if (count($components) == 1) {
-    echo '<span class="org-skill-term">' . $components[0] . '</span>';
-}
-else {
-    $time = strtolower($components[1]);
-    echo ('<span class="org-skill-term time-' . $time . '">');
-    echo $components[0];
-    echo '</span>';
-}
 ?>
+
+<?php if (count($components) == 1): ?>
+    <span class="org-skill-badge"> <?= $components[0]; ?> </span>
+<?php else: ?>
+    <span class="org-skill-wrapper">
+        <?php $time = strtolower($components[1]); ?>
+        <span class=<?= "'org-skill-clock clock-{$time}'" ?>>
+            <?php
+            $path = get_theme_file_uri('/lib/clock-o.svg');
+            echo file_get_contents($path); ?>
+        </span>
+        <span class="org-skill-badge">
+            <?= $components[0] ?>
+        </span>
+    </span>
+<?php endif; ?>
