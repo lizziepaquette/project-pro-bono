94a714b74b3a9e4562ad54635b43a2354292cc14
Rahul Gosain <gosain.r@gmail.com>
2017-08-28 14:36:00 -0400

    Improve checks before meta in single-organization

--- ppb-quotidian/single-organization.php
+++ ppb-quotidian/single-organization.php
@@ -1,43 +1,51 @@
 <?php get_header(); ?>
 <section class="content organization singular">
     <?php if (have_posts()): while(have_posts()): the_post(); ?>
-        <h1 class="org-name"><?= the_title(); ?></h3>
-        <div class="org-longform">
-            <div class="org-thumb">  <?= the_post_thumbnail(); ?> </div>
-            <div class="org-long-info">
-                <div class="org-long-meta inset">
-                    <div id="location">
-                        <strong> Located in: </strong>
-                        <?php
-                        $states = get_the_terms(get_the_ID(), 'state');
-                        foreach ($states as $state): ?>
-                            <a href="/state/<?= $state->slug ?>">
-                                <?= $state->name ?>
-                            </a>
-                        <? endforeach; ?>
-                    </div>
+    <h1 class="org-name"><?= the_title(); ?></h3>
+    <div class="org-longform">
+        <div class="org-thumb">  <?= the_post_thumbnail(); ?> </div>
+        <div class="org-long-info">
+            <div class="org-long-meta inset">
+                <?php
+                // Not all orgs will have a location - Political "orgs", for
+                // example are mainly links to resources. They don't need to
+                // have anything displayed here.
+                $states = get_the_terms(get_the_ID(), 'state');
 
-                    <div id="website">
-                        <?php
-                        $url = get_post_meta(get_the_ID(), 'learn_more_url', true);
-                        if ($url): ?>
-                            <strong> Learn More: </strong>
-                            <a target='_blank' href=<?= esc_url($url); ?>>
-                                <?= parse_url($url)['host']; ?>
-                            </a>
-                        <? endif; ?>
-
-                    </div>
+                if (!empty($state)): ?>
+                <div id="location">
+                    <strong> Located in: </strong>
+                    <?php foreach ($states as $state): ?>
+                        <a href="/state/<?= $state->slug ?>">
+                            <?= $state->name ?>
+                        </a>
+                    <? endforeach; ?>
                 </div>
+                <? endif; ?>
 
-                <div class="org-long-body">
-                    <?php the_content(); ?>
+                <?php
+                // Similarly, not all orgs will have states. I HOPE they all
+                // will (otherwise what's the point?) but the template should
+                // render OK even if they don't.
+                $url = get_post_meta(get_the_ID(), 'learn_more_url', true);
+
+                if ($url):?>
+                <div id="website">
+                    <strong> Learn More: </strong>
+                    <a target='_blank' href=<?= esc_url($url); ?>>
+                        <?= parse_url($url)['host']; ?>
+                    </a>
                 </div>
+                <? endif; ?>
+            </div>
+
+            <div class="org-long-body">
+                <?php the_content(); ?>
             </div>
         </div>
-
+    </div>
     <?php endwhile; else: ?>
-        <p>No content matched your criteria</p>
+    <p>No content matched your criteria</p>
     <?php endif ?>
 </section>
 <?php get_footer(); ?>
